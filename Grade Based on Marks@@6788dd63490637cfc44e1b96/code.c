cb2f51049b35d5cb3ae1cce6ba67228b2ae38070
aprankhunger <85071754+aprankhunger@users.noreply.github.com>
2025-02-06 16:42:07 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    a = a/10
+    switch(a){
+        case 10:
+        case 9:printf("A");break;
+        case 8:printf("B");break;
+        case 7:printf("c");break;
+        case 6:printf("D");break;
+        default: printf("F");
+    }
+    return 0;
+}
